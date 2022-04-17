5ac9ce8e355eb87a70f6fb81fc82ec35139d3ca7
Cenc <nacemerc11@gmail.com>
2022-04-17 22:18:29 +0200

    added flag to check if files are created

--- migrations/migration.ts
+++ migrations/migration.ts
@@ -13,6 +13,10 @@ import { getDefaultComponents } from './defaultValues';
 
 dotenv.config();
 
+let addDefaults = false;
+const flag = process.argv[2];
+if (flag === 'defaults') addDefaults = true;
+
 async function migrate() {
   if (!process.env.DB) {
     console.log('connection string doesnt exist');
@@ -40,10 +44,12 @@ async function migrate() {
     await connection.query(q);
   }
 
-  console.log('Creating default items');
-  const files = fs.readdirSync('./public/images');
-  for (const file of files) {
-    await connection.query(getDefaultComponents(file));
+  if (addDefaults) {
+    console.log('Creating default items');
+    const files = fs.readdirSync('./public/images');
+    for (const file of files) {
+      await connection.query(getDefaultComponents(file));
+    }
   }
 
   await connection.end();
