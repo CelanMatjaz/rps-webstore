a846ce63828305f1787a86f7b08884e4036c1ff4
Lidcer <thelidcer@gmail.com>
2022-04-22 14:40:58 +0200

    Fix quotes

--- src/server/router/users.ts
+++ src/server/router/users.ts
@@ -8,13 +8,13 @@ router.post('/register', async (req, res) => {
 
     const array = [username, name, last_name, mail, password, repeat_password];
     if (array.filter(e => !e).length) {
-        return res.status(400).send({error: "One of the fileds is missing"});
+        return res.status(400).send({error: 'One of the fileds is missing'});
     }
     if (!mail.includes('@')) {
-        return res.status(400).send({error: "Invalid mail"});
+        return res.status(400).send({error: 'Invalid mail'});
     }
     if (password !== repeat_password) {
-        return res.status(400).send({error: "Password do not match"});
+        return res.status(400).send({error: 'Password do not match'});
     }
 
     const user = await create({ username, name, last_name, mail, password })
@@ -25,12 +25,12 @@ router.post('/login', async (req, res) => {
     const { username, password, } = req.body;
 
     const array = [username, password];
-    if (array.filter(e => typeof e !== "string").length) {
-        return res.status(400).send({error: "One of the fileds is missing"});
+    if (array.filter(e => typeof e !== 'string').length) {
+        return res.status(400).send({error: 'One of the fileds is missing'});
     }
 
     const user = await findUserByUsernameWithPassword(username);
-    const r = () =>  res.json({error: "Wrong username or password!"});
+    const r = () =>  res.json({error: 'Wrong username or password!'});
 
     if (user) {
         const ok = comparePasswords(user.password, password);
@@ -48,10 +48,10 @@ router.post('/login', async (req, res) => {
 router.post('/logout', async (req, res) => { 
     if (req.session.userId) {
         req.session.destroy(() => {
-            res.json({data: "success"})
+            res.json({data: 'success'})
         });
     } else {
-        res.json({error: "Not logged in"})
+        res.json({error: 'Not logged in'})
     }
 });
 
@@ -60,8 +60,8 @@ router.post('/check', async (req, res) => {
         const user = await getUserById(req.session.userId);
         res.json({data: user})
     } else {
-        res.status(401).json({error: "Not looged in"})
+        res.status(401).json({error: 'Not looged in'})
     }
 });
-router.post('*', (req, res) => res.status(404).json({error: "Not found"}));
+router.post('*', (req, res) => res.status(404).json({error: 'Not found'}));
 export default router;
