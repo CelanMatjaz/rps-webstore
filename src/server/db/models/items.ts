a7587b5b2f1061e52ff9a045bbdd92b8d7944720
Matjaž Čelan <matjaz.celan.github@gmail.com>
2022-04-24 15:46:29 +0200

    updated routes

--- src/server/db/models/items.ts
+++ src/server/db/models/items.ts
@@ -5,6 +5,11 @@ import { sql } from 'slonik';
 const TABLE_NAME = 'items';
 
 export async function getAllItems():Promise<Item[]> {
-    const res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM ${TABLE_NAME}`)
+    const res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items`)
     return (res.rows ?? []) as Item[];
 }
+
+export async function getItemById(id: string) {
+    const res = await connection.maybeOne<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items WHERE id = ${id}`);
+    return res;
+} 
--- src/server/router/items.ts
+++ src/server/router/items.ts
@@ -1,12 +1,19 @@
 import express from 'express';
-import { getAllItems } from '../db/models/items';
+import { getAllItems, getItemById } from '../db/models/items';
 const router = express.Router();
 
 router.use(express.json());
+
 router.get('/all', async (req, res) => {
     const data = await getAllItems(); 
     res.json({ data });
 });
 
+router.get('/:id', async (req,res) => {
+    const data = await getItemById(req.params.id);
+    res.json({ data })
+})
+
 router.post('*', (req, res) => res.status(404).json({error: "Not found"}));
+
 export default router;
