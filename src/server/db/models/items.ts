333dd5d935dff89c3ef5af37f72d088d30fa8d7c
Lidcer <thelidcer@gmail.com>
2022-05-07 21:12:48 +0200

    Add pagination

--- src/server/db/models/items.ts
+++ src/server/db/models/items.ts
@@ -1,11 +1,18 @@
 import { connection } from '../connection';
 import { Item } from '../../../common/types';
-import { sql } from 'slonik';
+import { QueryResult, sql } from 'slonik';
 
 const TABLE_NAME = 'items';
 
-export async function getAllItems():Promise<Item[]> {
-    const res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items`)
+export async function getAllItems(offset?: number, limit?: number):Promise<Item[]> {
+    let res: QueryResult<Item>; 
+    const exist = (value: any) => !(value === undefined || value === null)
+    if (exist(offset) && exist(limit)) {
+        offset = offset * limit;
+        res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items OFFSET ${offset} LIMIT ${limit}`)
+    } else {
+        res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items`);
+    }
     return (res.rows ?? []) as Item[];
 }
 
--- src/server/router/items.ts
+++ src/server/router/items.ts
@@ -4,8 +4,25 @@ const router = express.Router();
 
 router.use(express.json());
 
+function correctNumber(sus: any) {
+    const susType = typeof sus;
+    if (susType === "string") {
+        sus = parseInt(sus, 10);
+    } else if (susType === "number") {
+        // ignored
+    } else {
+        return undefined;
+    }
+    if (isNaN(sus)) {
+        return undefined;
+    }
+    return Math.round(Math.abs(sus));
+}
+
 router.get('/all', async (req, res) => {
-    const data = await getAllItems(); 
+    const offset = correctNumber(req.query.page);
+    const limit = correctNumber(req.query.page_size);
+    const data = await getAllItems(offset, limit); 
     res.json({ data });
 });
 
@@ -13,6 +30,7 @@ router.get('/:id', async (req,res) => {
     const data = await getItemById(req.params.id);
     res.json({ data })
 })
+
 
 router.post('*', (req, res) => res.status(404).json({error: "Not found"}));
 
