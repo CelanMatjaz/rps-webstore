ee701bb334b85a84594bef13003419de6f556aaf
Lidcer <thelidcer@gmail.com>
2022-05-07 21:19:28 +0200

    Make page start on index 1

--- src/server/db/models/items.ts
+++ src/server/db/models/items.ts
@@ -8,7 +8,10 @@ export async function getAllItems(offset?: number, limit?: number):Promise<Item[
     let res: QueryResult<Item>; 
     const exist = (value: any) => !(value === undefined || value === null)
     if (exist(offset) && exist(limit)) {
-        offset = offset * limit;
+        if (offset === 0) {
+            offset++;
+        }
+        offset = (offset - 1) * limit;
         res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items OFFSET ${offset} LIMIT ${limit}`)
     } else {
         res = await connection.query<Item>(sql`SELECT id, quantity, name, price, discount, description, img_path, created_at, modified_at FROM items`);
